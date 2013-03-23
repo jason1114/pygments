725f03f8874fed9f5e003368c961defc3001648b
Marco Otte-Witte <mottewitte@rim.com>
2013-03-23 12:33:15 +0100

    access request data correctly

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def index():
     if request.method == 'GET':
         return open('README', 'r').read()
     elif request.method == 'POST':
-        return pygmentize(self.request.get("lang"), self.request.get("code"))
+        return pygmentize(request.form("lang"), request.form("code"))
 
 def pygmentize(lang, code):
   lexer     = get_lexer_by_name(lang)
